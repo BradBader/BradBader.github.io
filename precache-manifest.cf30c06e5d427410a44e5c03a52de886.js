504c7a3c80a6882dc10103efa1aea4b229c5d85d
BBadoo <bbader0806@gmail.com>
2019-06-18 16:52:33 -0400

    Updates

---
+++ precache-manifest.cf30c06e5d427410a44e5c03a52de886.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88e3d64f7a6b8a730bdd39c32cde9a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cded9728a8f66a2b303c",
+    "url": "/static/css/main.1429c291.chunk.css"
+  },
+  {
+    "revision": "8c307e2d1be7b5981c86",
+    "url": "/static/js/2.9062445e.chunk.js"
+  },
+  {
+    "revision": "cded9728a8f66a2b303c",
+    "url": "/static/js/main.edddc412.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b3d43b9988ae2ad19dc96610213b744d",
+    "url": "/static/media/collin-armstrong-189527-unsplash.b3d43b99.jpg"
+  },
+  {
+    "revision": "cdf4bf826a76b0531a1dbe211e8449ba",
+    "url": "/static/media/imgone.cdf4bf82.jpg"
+  },
+  {
+    "revision": "712cebe47520ae9fe1dbead17af42e5b",
+    "url": "/static/media/sideCoversCrop.712cebe4.jpg"
+  }
+]);
